a2481a0c4f82396a0d8f4ed55a4b22607192f32d
ShivamGaikwad200 <shivamgaikwad200@gmail.com>
2024-10-02 10:45:20 +0530

    finished project

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var canaddoperation=false
-    private var canadddecimal=false
+    private var canadddecimal=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
